f5b6df56d5be9c68d0ac67370ab2ced37cca20ad
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-04-27 10:54:46 +0200

    modifica AcquistoTesserePermesso

--- src/main/java/Azioni/AcquistoTesseraPermesso.java
+++ src/main/java/Azioni/AcquistoTesseraPermesso.java
@@ -1,4 +1,12 @@
 package Azioni;
 
+import Game.Partita;
+
 public class AcquistoTesseraPermesso extends AzionePrincipale {
+
+	@Override
+	public boolean eseguiAzione(Partita partita) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
