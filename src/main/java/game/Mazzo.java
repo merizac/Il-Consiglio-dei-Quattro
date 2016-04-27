1066842844c36e60993237e6b41f4e39098a0d7f
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-04-27 18:52:31 +0200

    classe Mazzo<T>

--- src/main/java/game/Mazzo.java
+++ src/main/java/game/Mazzo.java
@@ -1,20 +1,49 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Mazzo<T> {
 
 	private ArrayList<T> carte;
+	
+	
 
-	public ArrayList<T> pescaCarte() {
-		throw new UnsupportedOperationException();
+	/**
+	 * @param carte
+	 */
+	public Mazzo(ArrayList<T> carte) {
+		this.carte = carte;
 	}
 
+	/**
+	 * @return the carte
+	 */
+	public ArrayList<T> getCarte() {
+		return carte;
+	}
+
+	/**
+	 * 
+	 * @return carta pescata
+	 */
+	public T pescaCarte() {
+		return this.carte.remove(0);
+	}
+
+	/**
+	 * shuffle the deck
+	 */
 	public void mescolaCarte() {
-		throw new UnsupportedOperationException();
+		Collections.shuffle(carte);
 	}
 
+	/**
+	 * Add the card at the end of the deck
+	 * @param carta
+	 */
 	public void aggiungiCarta(T carta) {
-		throw new UnsupportedOperationException();
+		carte.add(carta);
 	}
 }
