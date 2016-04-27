780b5d302414a863b46c0c92990ba12560b75597
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-04-27 15:12:29 +0200

    classe colore

--- src/main/java/game/Colore.java
+++ src/main/java/game/Colore.java
@@ -2,12 +2,18 @@ package game;
 
 public class Colore {
 	private final String colore;
-
+	/**
+	 * the construct of the class Colore set the variable colore at the parameter passed
+	 * @param colore
+	 */
 	public Colore(String colore){
 	
 		this.colore=colore;
 	}
-
+	/**
+	 * return the value of the variable colore
+	 * @return colore 
+	 */
 	public String getColore() {
 		return colore;
 	}
