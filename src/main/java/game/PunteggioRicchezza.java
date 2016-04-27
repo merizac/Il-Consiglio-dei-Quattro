610334db6fa3b16cc9e69f78ec684634181d27a9
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-04-27 15:29:50 +0200

    classe PunteggioRicchezza

--- src/main/java/game/PunteggioRicchezza.java
+++ src/main/java/game/PunteggioRicchezza.java
@@ -3,16 +3,35 @@ package game;
 public class PunteggioRicchezza {
 
 	private int puntiRicchezza;
+	
+
+	/**
+	 * @param puntiRicchezza
+	 */
+	public PunteggioRicchezza(int puntiRicchezza) {
+		super();
+		this.puntiRicchezza = puntiRicchezza;
+	}
 
 	public int getPuntiRicchezza() {
 		return this.puntiRicchezza;
 	}
+	
 
 	public void aggiungiPuntiRicchezza(int puntiRicchezza) {
-		throw new UnsupportedOperationException();
+		this.puntiRicchezza = this.puntiRicchezza + puntiRicchezza;
 	}
 
-	public void togliiPuntiR(int puntiRicchezza) {
-		throw new UnsupportedOperationException();
+	/**
+	 * Decrease puntiRicchezza
+	 * @param puntiRicchezza
+	 * @return true if it is possible to decrease puntiRicchezza, otherwise return false
+	 */
+	public Boolean togliPuntiRicchezza(int puntiRicchezza) {
+		if(this.puntiRicchezza < puntiRicchezza) return false;
+		else{
+			this.puntiRicchezza = this.puntiRicchezza - puntiRicchezza;
+			return true;
+		}
 	}
 }
