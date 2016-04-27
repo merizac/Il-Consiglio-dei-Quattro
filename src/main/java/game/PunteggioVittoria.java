47066e90a5db56df062f4b8be700e0d705b0c4a7
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-04-27 15:22:49 +0200

    classe PunteggioVittoria

--- src/main/java/game/PunteggioVittoria.java
+++ src/main/java/game/PunteggioVittoria.java
@@ -3,12 +3,21 @@ package game;
 public class PunteggioVittoria {
 
 	private int puntiVittoria;
+	
+	//costruttore
+	public PunteggioVittoria(int puntiVittoria) {
+		this.puntiVittoria = puntiVittoria;
+	}
 
 	public int getPuntiVittoria() {
 		return this.puntiVittoria;
 	}
-
-	public void aggiungiPuntiVittoria(int puntiVIttoria) {
-		throw new UnsupportedOperationException();
+	/**
+	 * add puntiVittoria (param) to the local variable puntiVittoria
+	 * @param puntiVittoria
+	 */
+	public void aggiungiPuntiVittoria(int puntiVittoria) {
+		this.puntiVittoria = this.puntiVittoria + puntiVittoria;
 	}
+	
 }
