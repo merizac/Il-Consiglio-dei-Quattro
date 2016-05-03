cc9c06ed156d5da6076c87cf466c2f6d7eda7c66
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-05-04 00:12:48 +0200

    add equals and hashcode in class colore

--- src/main/java/game/Colore.java
+++ src/main/java/game/Colore.java
@@ -17,5 +17,36 @@ public class Colore {
 	public String getColore() {
 		return colore;
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((colore == null) ? 0 : colore.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Colore other = (Colore) obj;
+		if (colore == null) {
+			if (other.colore != null)
+				return false;
+		} else if (!colore.equals(other.colore))
+			return false;
+		return true;
+	}
+	
+	
 
 }
