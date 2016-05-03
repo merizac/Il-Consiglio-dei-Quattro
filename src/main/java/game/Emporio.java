5d01efec9a549d94dbb01f8ddd22d28e4571a8f5
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-05-04 00:39:47 +0200

    add equals and hashcode to class Emporio

--- src/main/java/game/Emporio.java
+++ src/main/java/game/Emporio.java
@@ -20,4 +20,39 @@ public class Emporio {
 	public Colore getColore() {
 		return this.colore;
 	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((colore == null) ? 0 : colore.hashCode());
+		return result;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Emporio other = (Emporio) obj;
+		if (colore == null) {
+			if (other.colore != null)
+				return false;
+		} else if (!colore.equals(other.colore))
+			return false;
+		return true;
+	}
+	
+	
 }
