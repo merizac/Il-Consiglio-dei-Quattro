0b50545afa9bf3837cc70f9730e022ab44b332a8
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-05-22 10:28:33 +0200

    error

--- src/main/java/bonus/BonusNobiltà.java
+++ src/main/java/bonus/BonusNobiltà.java
@@ -2,11 +2,12 @@ package bonus;
 import controller.Partita;
 
 public abstract class BonusNobiltà extends Bonus {
-
+	
 	public BonusNobiltà() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	
+
 	public abstract void usaBonus(Partita partita);
 
 }
