d17c8c0475adcd1c959f5d94ea6e7f70083289f5
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-06-02 15:40:15 +0200

    new state for the end of the game

---
+++ src/main/java/game/macchinaStati/StatoFinePartita.java
@@ -0,0 +1,13 @@
+package game.macchinaStati;
+
+import java.util.List;
+
+public class StatoFinePartita implements Stato {
+
+	@Override
+	public List<String> getAzioni() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
