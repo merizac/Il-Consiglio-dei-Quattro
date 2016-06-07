520582d0b52d3106aba7c5bfbb6e540843ed31a7
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-06-07 23:29:02 +0200

    DTO-Regione, PlanciaRe and Re

---
+++ src/test/java/gameDTO/gameDTO/RegioneDTOTest.java
@@ -0,0 +1,129 @@
+package gameDTO.gameDTO;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import bonus.Bonus;
+import bonus.BonusAiutanti;
+import bonus.BonusPuntiVittoria;
+import game.Consigliere;
+import game.GameState;
+import game.Giocatore;
+import game.TesseraPermesso;
+
+public class RegioneDTOTest {
+
+	static GameState gameState;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ArrayList<Giocatore> giocatori=new ArrayList<>();
+		Giocatore giocatore=new Giocatore("Giocatore");
+		giocatori.add(giocatore);
+		gameState=new GameState();
+		gameState.start(giocatori);
+	}
+
+	@Test
+	public void testGetNome() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		
+		regioneDTO.setNome("A");
+		assertTrue("A"==regioneDTO.getNome());
+
+	}
+
+	@Test
+	public void testSetNome() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		
+		regioneDTO.setNome("A");
+		assertTrue("A"==regioneDTO.getNome());
+	}
+
+	@Test
+	public void testGetTesserePermessoScoperte() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		ArrayList<TesseraPermessoDTO> tesserePermessoScoperte=new ArrayList<>();
+		for(TesseraPermesso t: gameState.getRegioni().get(0).getTesserePermessoScoperte()){
+			TesseraPermessoDTO tp=new TesseraPermessoDTO();
+			tp.inizializza(t);
+			tesserePermessoScoperte.add(tp);
+		}
+		regioneDTO.setTesserePermessoScoperte(tesserePermessoScoperte);
+		assertTrue(tesserePermessoScoperte==regioneDTO.getTesserePermessoScoperte());
+	}
+
+	@Test
+	public void testSetTesserePermessoScoperte() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		ArrayList<TesseraPermessoDTO> tesserePermessoScoperte=new ArrayList<>();
+		for(TesseraPermesso t: gameState.getRegioni().get(0).getTesserePermessoScoperte()){
+			TesseraPermessoDTO tp=new TesseraPermessoDTO();
+			tp.inizializza(t);
+			tesserePermessoScoperte.add(tp);
+		}
+		regioneDTO.setTesserePermessoScoperte(tesserePermessoScoperte);
+		assertTrue(tesserePermessoScoperte==regioneDTO.getTesserePermessoScoperte());
+	}
+
+	@Test
+	public void testGetBonusRegione() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		
+		Bonus bonus=new BonusPuntiVittoria(2);
+		regioneDTO.setBonusRegione(bonus);
+		assertTrue(bonus==regioneDTO.getBonusRegione());
+	}
+
+	@Test
+	public void testSetBonusRegione() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		
+		Bonus bonus=new BonusPuntiVittoria(2);
+		regioneDTO.setBonusRegione(bonus);
+		assertTrue(bonus==regioneDTO.getBonusRegione());
+	}
+
+	@Test
+	public void testGetBalcone() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		ArrayList<ConsigliereDTO> balcone=new ArrayList<>();
+		for(Consigliere c: gameState.getRegioni().get(0).getBalcone().getConsigliere()){
+			ConsigliereDTO consigliereDTO=new ConsigliereDTO();
+			consigliereDTO.inizializza(c);
+			balcone.add(consigliereDTO);
+		}
+		regioneDTO.setBalcone(balcone);
+		
+		assertTrue(balcone==regioneDTO.getBalcone());
+	}
+
+	@Test
+	public void testSetBalcone() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		ArrayList<ConsigliereDTO> balcone=new ArrayList<>();
+		for(Consigliere c: gameState.getRegioni().get(0).getBalcone().getConsigliere()){
+			ConsigliereDTO consigliereDTO=new ConsigliereDTO();
+			consigliereDTO.inizializza(c);
+			balcone.add(consigliereDTO);
+		}
+		regioneDTO.setBalcone(balcone);
+		
+		assertTrue(balcone==regioneDTO.getBalcone());
+	}
+
+	@Test
+	public void testInizializza() {
+		RegioneDTO regioneDTO=new RegioneDTO();
+		regioneDTO.inizializza(gameState.getRegioni().get(0));
+		
+		assertEquals(gameState.getRegioni().get(0).getNome(), regioneDTO.getNome());
+		assertTrue(gameState.getRegioni().get(0).getBonusRegione()==regioneDTO.getBonusRegione());
+	}
+
+}
