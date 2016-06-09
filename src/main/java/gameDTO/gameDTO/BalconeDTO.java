4ae3127192cae0812b6886cb3df742db1a3baa2f
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-06-09 17:39:29 +0200

    fixed problem with balconedto

--- src/main/java/gameDTO/gameDTO/BalconeDTO.java
+++ src/main/java/gameDTO/gameDTO/BalconeDTO.java
@@ -1,29 +1,35 @@
 package gameDTO.gameDTO;
 
+import java.io.Serializable;
 import java.util.ArrayList;
-
+import java.util.List;
 import game.Balcone;
 import game.Consigliere;
 
-public class BalconeDTO {
+public class BalconeDTO implements Serializable{
 
-	private ArrayList<ConsigliereDTO> consiglieri;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6523476700234911129L;
+	private List<ConsigliereDTO> consiglieri;
 
 	/**
 	 * @return the consiglieri
 	 */
-	public ArrayList<ConsigliereDTO> getConsiglieri() {
+	public List<ConsigliereDTO> getConsiglieri() {
 		return consiglieri;
 	}
 
 	/**
 	 * @param consiglieri the consiglieri to set
 	 */
-	public void setConsiglieri(ArrayList<ConsigliereDTO> consiglieri) {
+	public void setConsiglieri(List<ConsigliereDTO> consiglieri) {
 		this.consiglieri = consiglieri;
 	}
 	
 	public void inizializza(Balcone balcone){
+		this.consiglieri=new ArrayList<>();
 		for(Consigliere c: balcone.getConsigliere()){
 			ConsigliereDTO consigliereDTO = new ConsigliereDTO();
 			consigliereDTO.inizializza(c);
