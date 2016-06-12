c821da844da163f16c3cd11b8a33472dc89bbc85
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-06-12 21:35:48 +0200

    stato bonus test

--- src/test/java/server/model/macchinaStati/StatoBonusTest.java
+++ src/test/java/server/model/macchinaStati/StatoBonusTest.java
@@ -72,10 +72,9 @@ public class StatoBonusTest {
 		Stato11 stato11=new Stato11(gameState);
 		StatoBonus statoBonus=new StatoBonus(gameState, stato11);
 		gameState.setStato(statoBonus);
-//percorso nobilta non deve avere il bonus
 		statoBonus.transizioneBonus(gameState);
 
-		assertTrue(gameState.getStato() instanceof Stato11);
+		assertTrue(gameState.getStato() instanceof StatoBonus);
 	}
 	
 	@Test
