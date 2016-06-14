d2d19a17eeeec75b1be533745e3a7018de6395df
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-06-14 15:06:59 +0200

    modified passaBonus

--- src/main/java/server/model/azioni/azioniBonus/PassaBonus.java
+++ src/main/java/server/model/azioni/azioniBonus/PassaBonus.java
@@ -8,7 +8,6 @@ import server.model.game.GameState;
 public class PassaBonus extends Azione{
 
 	private Azione bonus;
-	private final int ID = 16;
 
 	@Override
 	public void eseguiAzione(GameState gameState) {
@@ -36,36 +35,5 @@ public class PassaBonus extends Azione{
 	public void setBonus(Azione bonus) {
 		this.bonus = bonus;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ID;
-		return result;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PassaBonus other = (PassaBonus) obj;
-		if (ID != other.ID)
-			return false;
-		return true;
-	}
-	
-		
-	
 	
 }
