06e1a0dbdbce0523eb36448353feca2ec35e46ea
Nicola Sosio <sosio.nicola94@tiscali.it>
2016-06-16 00:09:37 +0200

    modified gui

--- src/main/java/client/grafica/gui/GUIGameController.java
+++ src/main/java/client/grafica/gui/GUIGameController.java
@@ -1,18 +1,21 @@
 package client.grafica.gui;
 
-import java.util.List;
-
 import common.gameDTO.GameStateDTO;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 
 public class GUIGameController {
-	
+
 	private GameStateDTO gameStateDTO;
 
+	@FXML
+	private AnchorPane arkon;
+	@FXML
+	private AnchorPane castrum;
+
 	/**
-	 * @param gameStateDTO the gameStateDTO to set
+	 * @param gameStateDTO
+	 *            the gameStateDTO to set
 	 */
 	public void setGameStateDTO(GameStateDTO gameStateDTO) {
 		this.gameStateDTO = gameStateDTO;
