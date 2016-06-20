b1f055b40fdb87cf09c716ea63c8de7921e1c9a7
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-06-20 19:05:07 +0200

    game

--- src/main/java/client/grafica/gui/GUIGameController.java
+++ src/main/java/client/grafica/gui/GUIGameController.java
@@ -496,6 +496,10 @@ public class GUIGameController {
 						hbox.setSpacing(5);
 						tab.setText(g.getNome());
 						hbox.relocate(20, 5);
+						vbox.getChildren().add(hbox);
+						tab.setContent(vbox);
+						giocatori.getTabs().add(tab);
+
 
 						Pane pane1=new Pane();
 						ImageView puntiV=new ImageView();
@@ -505,13 +509,9 @@ public class GUIGameController {
 						Text npuntiV=new Text();
 						npuntiV.setText(Integer.toString(g.getPunteggioVittoria()));
 						npuntiV.relocate(10, 10);
-						hbox.setSpacing(10);						
 						pane1.getChildren().add(puntiV);
 						pane1.getChildren().add(npuntiV);
 						hbox.getChildren().add(pane1);
-						vbox.getChildren().add(hbox);
-						tab.setContent(vbox);
-						giocatori.getTabs().add(tab);
 						
 						Pane pane2=new Pane();
 						ImageView puntiR=new ImageView();
@@ -521,13 +521,9 @@ public class GUIGameController {
 						Text npuntiR=new Text();
 						npuntiR.setText(Integer.toString(g.getPunteggioRicchezza()));
 						npuntiR.relocate(10, 10);
-						hbox.setSpacing(10);						
 						pane2.getChildren().add(puntiR);
 						pane2.getChildren().add(npuntiR);
 						hbox.getChildren().add(pane2);
-						vbox.getChildren().add(hbox);
-						tab.setContent(vbox);
-						giocatori.getTabs().add(tab);
 			
 						Pane pane3=new Pane();
 						ImageView aiutanti=new ImageView();
@@ -537,15 +533,21 @@ public class GUIGameController {
 						Text naiutanti=new Text();
 						naiutanti.setText(Integer.toString(g.getAiutanti()));
 						naiutanti.relocate(10, 10);
-						hbox.setSpacing(10);						
 						pane3.getChildren().add(aiutanti);
 						pane3.getChildren().add(naiutanti);
 						hbox.getChildren().add(pane3);
-						vbox.getChildren().add(hbox);
-						tab.setContent(vbox);
-						giocatori.getTabs().add(tab);
 			
-						
+						Pane pane4=new Pane();
+						ImageView emporio=new ImageView();
+						emporio.setImage(new Image(getClass().getResource("css/Point.png").toExternalForm()));
+						emporio.setPreserveRatio(true);
+						emporio.setFitHeight(40);
+						Text empori=new Text();
+						empori.setText(Integer.toString(g.getEmpori()));
+						empori.relocate(10, 10);
+						pane4.getChildren().add(emporio);
+						pane4.getChildren().add(empori);
+						hbox.getChildren().add(pane4);
 					}
 				}
 				catch(Exception e){
