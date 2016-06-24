1a6f37408cc5169f396a0ee750955cf0488fcaf1
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-06-24 20:13:33 +0200

    tappato buchi

--- src/main/java/client/grafica/gui/GUIMarketController.java
+++ src/main/java/client/grafica/gui/GUIMarketController.java
@@ -1,17 +1,9 @@
 package client.grafica.gui;
 
-import java.rmi.RemoteException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import common.azioniDTO.AzioneAcquistoDTO;
-import common.azioniDTO.AzioneDTO;
-import common.azioniDTO.AzioneOffertaDTO;
-import common.azioniDTO.AzioneParametri;
-import common.azioniDTO.PassaDTO;
 import common.gameDTO.GameStateDTO;
+import common.gameDTO.OffertaDTO;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -20,11 +12,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import utility.AzioneNonEseguibile;
 import utility.Utils;
 
 public class GUIMarketController {
-
+	
+	
 	private GUI gui;
 	private GameStateDTO gameStateDTO;
 	@FXML
@@ -43,28 +35,19 @@ public class GUIMarketController {
 	private Button passa;
 	@FXML
 	private TextField prezzo;
-	@FXML
-	private Button ok;
-
-	private List<Button> azioni;
-
-	public void inizializza() {
-		acquisto.setUserData(new AzioneAcquistoDTO());
-		offerta.setUserData(new AzioneOffertaDTO());
-		passa.setUserData(new PassaDTO());
-		azioni = Arrays.asList(acquisto, offerta, passa);
-	}
-
-	public VBox getOfferte() {
+	
+	
+	public VBox getOfferte(){
 		return offerte;
 	}
-
+	
 	/**
 	 * @return the aiutanti
 	 */
 	public HBox getAiutanti() {
 		return aiutanti;
 	}
+
 
 	/**
 	 * @return the cartePolitica
@@ -73,6 +56,7 @@ public class GUIMarketController {
 		return cartePolitica;
 	}
 
+
 	/**
 	 * @return the tesserePermesso
 	 */
@@ -80,9 +64,9 @@ public class GUIMarketController {
 		return tesserePermesso;
 	}
 
+
 	/**
-	 * @param gui
-	 *            the gui to set
+	 * @param gui the gui to set
 	 */
 	public void setGui(GUI gui) {
 		this.gui = gui;
@@ -96,84 +80,39 @@ public class GUIMarketController {
 	}
 
 	/**
-	 * @param gameStateDTO
-	 *            the gameStateDTO to set
+	 * @param gameStateDTO the gameStateDTO to set
 	 */
 	public void setGameStateDTO(GameStateDTO gameStateDTO) {
 		this.gameStateDTO = gameStateDTO;
 	}
-
-	/**
-	 * @return the ok
-	 */
-	public Button getOk() {
-		return ok;
+	
+	public void handleAzione(ActionEvent event){
+		
 	}
-
-	public void handleAzione(ActionEvent event) {
-		ExecutorService executor = Executors.newSingleThreadExecutor();
-		executor.submit(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					for (Button b : azioni)
-						b.setDisable(true);
-					AzioneDTO azioneDTO = gameStateDTO.getAzioniDisponibili().stream()
-							.filter(a -> a.getClass()
-									.equals(((AzioneDTO) ((Button) event.getSource()).getUserData()).getClass()))
-							.findAny().orElse(null);
-
-					if (azioneDTO == null) {
-						// gui.mostraMessaggio("L'azione non esiste \nInserire
-						// un'azione valida");
-						for (Button b : azioni)
-							b.setDisable(false);
-						return;
-					} else if (azioneDTO instanceof AzioneParametri) {
-						try {
-							((AzioneParametri) azioneDTO).parametri().setParametri(gui, gameStateDTO);
-						} catch (AzioneNonEseguibile e) {
-							// gui.mostraMessaggio(e.getMessage());
-							return;
-						}
-					}
-					try {
-						gui.getConnessione().inviaAzione(azioneDTO);
-						for (Button b : azioni)
-							b.setDisable(false);
-					} catch (RemoteException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+	
+	public void handleOfferta(Event event){
+		synchronized (gui.getLock()) {
+			gui.setParametro(((ImageView)event.getSource()).getUserData());
+			gui.getLock().notifyAll();
+		}
+	}
+	
+	public void handlePrezzo(Event event){
+		synchronized (gui.getLock()) {
+			String prezzo=((TextField)event.getSource()).getText();
+			if(Utils.isNumeric(prezzo)){
+				gui.setParametro(prezzo);
+				gui.notifyAll();
 			}
-
-		});
-	}
-
-	public void handleOfferta(Event event) {
-		synchronized (gui.getLock()) {
-			gui.setParametro(((ImageView) event.getSource()).getUserData());
-			((ImageView) event.getSource()).setDisable(true);
-			((ImageView) event.getSource()).setOpacity(0.5);
-			gui.getLock().notifyAll();
 		}
 	}
-
-	public void handlePrezzo(Event event) {
-		ok.setDisable(false);
+	
+	
+	public void inizializza(){
+		
 	}
-
-	public void handleOk(ActionEvent event) {
-		synchronized (gui.getLock()) {
-			String prezzo = this.prezzo.getText();
-			gui.setParametro(prezzo);
-			gui.getLock().notifyAll();
-			ok.setDisable(true);
-		}
+	
+	public Button getOk(){
+		return new Button();
 	}
-
 }
