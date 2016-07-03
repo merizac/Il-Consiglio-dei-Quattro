3c7fe231331b02e9c6b7e4750f45778b1c96b734
mariachiara <mariachiara.zaccardi@mail.polimi.it>
2016-07-03 22:49:55 +0200

    modify

--- src/main/java/client/grafica/gui/GUI.java
+++ src/main/java/client/grafica/gui/GUI.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
